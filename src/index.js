0462122742e66493eeb38439000e77eb49a35264
Siraj Alam <sirajalam@Mithyas-MacBook-Air.local>
2018-07-06 11:26:53 +0530

    Completed without edit functionality

--- src/index.js
+++ src/index.js
@@ -6,34 +6,42 @@ import './index.css';
 class MaintainList extends React.Component{
     constructor(props){
         super(props);
+        this.tasks = [
+            {name: 'This is done', done: false},
+            {name: 'This is not', done: true}
+        ];
         this.state = {
-            items: [
-                {name: 'This is done.', done: true},
-                {name: 'This isn\'t done', done: false }
-            ],
-
+            items: this.tasks
         }   
         this.addItem = this.addItem.bind(this);
         this.delItem = this.delItem.bind(this);
+        this.toggleDone = this.toggleDone.bind(this);
     }
 
     addItem(task_name){
-        let new_items = this.state.items.slice();
-        new_items.unshift({name: task_name, done: false});
-        this.setState({items: new_items});
+        this.tasks.unshift({name: task_name, done: false});
+        this.setState({items: this.tasks});
     }
 
     delItem(item_index){
-        let new_items = this.state.items.slice();
-        new_items.splice(item_index, 1);
-        this.setState({items: new_items});
+        this.tasks.splice(item_index, 1);
+        this.setState({items: this.tasks});
+    }
+
+    toggleDone(item_index){
+        var todo = this.tasks[item_index];
+        this.tasks.splice(item_index, 1);
+        todo.done = !todo.done;
+        todo.done?this.tasks.push(todo):this.tasks.unshift(todo);
+        this.setState({items: this.tasks});   
     }
 
     render(){
         return (
-            <div id="app">
+            <div id="app" className="container">
+                <h1>To Do </h1> <br />
                 <AddTask  add={this.addItem} />
-                <ShowTaskManager list={this.state.items} del={this.delItem} />
+                <ShowTaskManager list={this.state.items} del={this.delItem} toggleDone={this.toggleDone}/>
             </div>
         );
     }
@@ -51,9 +59,9 @@ class AddTask extends React.Component{
     }
     render(){
         return (
-            <form className="row" onSubmit={this.handleSubmit}>
-                <input type="text" ref="itemName" className="col-11" />
-                <button type="submit" value="submit" className="btn btn-info col-1">+</button>
+            <form className="form-inline" onSubmit={this.handleSubmit}>
+                <input type="text" className="form-control col-sm-11" ref="itemName" />
+                <button type="submit" class="btn btn-primary col-sm-1"><i className="fas fa-plus"></i></button>
             </form>
         );
     }
@@ -66,25 +74,26 @@ class ShowTaskManager extends React.Component{
     render(){
         let listSet = this.props.list.map(
             (item, index) => {
-                let workStatusClassName = this.done ? 'done': 'undone';
-                let workStatusIcon = this.done ? 'fa-circle-checked' : 'fa-circle';
+                let workStatusClassName = item.done ? 'done': 'undone';
+                let workStatusIcon = item.done ? 'fas fa-check-circle' : 'fas fa-circle';
                 return (
-                    <li className="row" key={index}> 
-                        <i className="fa col-md-1" className={workStatusIcon}></i>
-                        <span className="col-md-10">{item.name}</span>
-                        <i className="fa fa-trash-alt col-md-1" onClick={() => {this.props.del(index);}}></i>
+                    <li className="list-group-item" key={index}> 
+                        <span className="col-md-1" onClick={() => {this.props.toggleDone(index)}}><i className={workStatusIcon}></i></span>
+                        <span className={`col-md-10 ${workStatusClassName}`} >{item.name}</span>
+                        <span className="col-md-1" onClick={() => {this.props.del(index);}}><i className="fas fa-times"></i></span>
                     </li>
                 )
             }
         )
         return (
-            <ul>{listSet}</ul>
+            <ul id="todo-list" className="list-group">{listSet}</ul>
         );
     }
 }
 ShowTaskManager.propTypes = {
     list: PropTypes.array, //List will contain the list of tasks. Each task contains index, name and status if the task is done or not
-    del: PropTypes.func
+    del: PropTypes.func,
+    toggleDone: PropTypes.func
 }
 
 const TodoApp = () => {
